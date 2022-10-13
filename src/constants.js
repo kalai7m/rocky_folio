1df8be9c8d3df184edfa13c1196e3ac1667fe050
kalai7m <kalaivani7m@gmail.com>
2022-10-13 17:30:47 +0530

    config and constants

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const media = {
+  facebook: "abcd",
+  instagram: "abcd",
+  linkedin: "as",
+  mail: "bharathrocky@gmail.com",
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '576px',
+      'md': '768px',
+      'lg': '892px',
+      'xl': '1024px',
+      '2xl': '1200px',
+      '3xl': '1440px',
+      '4xl': '1532px',
+    },
+    extend: {
+      colors: {
+        'darkBlack': '#081219',
+        'grassGreen' : {
+          100: '#1CC98A',
+          200: '#0F855B',
+          300: '#2BDA80',
+        },
+      },
+      backgroundImage: {
+        'profile': "url('/src/assets/profile.png')"
+      }
+    },
   },
   plugins: [],
-}
+};
