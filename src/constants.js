f3b10c620bb5eb46f029ea9070140d3f072db0e7
kalai7m <kalaivani7m@gmail.com>
2022-11-05 13:03:16 +0530

    constant file updatge

--- src/constants.js
+++ src/constants.js
@@ -6,3 +6,60 @@ export const media = {
   behance: "#",
   dribble: "#",
 };
+
+export const PROJECT_DATA = [
+  {
+    _id: "t1",
+    label: "UI Design",
+    title: "Weather App",
+    img_url: "weather_app.png",
+    view_link: "#",
+    isActive: true,
+    index: 0,
+  },
+  {
+    _id: "t1",
+    label: "UI Design",
+    title: "Sift App",
+    img_url: "sift_app.png",
+    view_link: "#",
+    isActive: true,
+    index: 1,
+  },
+  {
+    _id: "t3",
+    label: "Logo Design",
+    title: "Sift App",
+    img_url: "sift_app.png",
+    view_link: "#",
+    isActive: false,
+    index: 2,
+  },
+  // {
+  //   _id: "t4",
+  //   label: "Logo Design",
+  //   title: "Sift App",
+  //   img_url: "weather_app.png",
+  //   view_link: "#",
+  //   isActive: false,
+  //   index: 3,
+  // },
+];
+
+export const TAB_DATA = [
+  {
+    _id: "t1",
+    label: "UI Design",
+    active: true,
+  },
+  {
+    _id: "t2",
+    label: "UX Design",
+    active: false,
+  },
+  {
+    _id: "t3",
+    label: "Logo Design",
+    active: false,
+  },
+];
