6caa1b7ac26a80cc4532e26d7c95620772678674
kalai7m <kalaivani7m@gmail.com>
2022-11-13 11:16:35 +0530

    constant file update

--- src/constants.js
+++ src/constants.js
@@ -7,74 +7,17 @@ export const media = {
   dribble: "#",
 };
 
-export const PROJECT_DATA = [
-  {
-    _id: "t1",
-    label: "UI Design",
-    title: "Weather App",
-    img_url: "weather_app.png",
-    view_link: "#",
-    isActive: true,
-    index: 0,
-  },
-  {
-    _id: "t1",
-    label: "UI Design",
-    title: "Sift App",
-    img_url: "sift_app.png",
-    view_link: "#",
-    isActive: true,
-    index: 1,
-  },
-  {
-    _id: "t1",
-    label: "Logo Design",
-    title: "Sift App",
-    img_url: "sift_app.png",
-    view_link: "#",
-    isActive: false,
-    index: 2,
-  },
-  {
-    _id: "t2",
-    label: "Logo Design",
-    title: "Sift App",
-    img_url: "weather_app.png",
-    view_link: "#",
-    isActive: true,
-    index: 0,
-  },
-  {
-    _id: "t3",
-    label: "UI Design",
-    title: "Sift App",
-    img_url: "sift_app.png",
-    view_link: "#",
-    isActive: true,
-    index: 0,
-  },
-  {
-    _id: "t2",
-    label: "Logo Design",
-    title: "Sift App",
-    img_url: "weather_app.png",
-    view_link: "#",
-    isActive: true,
-    index: 1,
-  },
-];
-
 export const TAB_DATA = [
   {
-    _id: "t1",
+    _id: "uidesign",
     label: "UI Design",
   },
   {
-    _id: "t2",
+    _id: "uxdesign",
     label: "UX Design",
   },
   {
-    _id: "t3",
+    _id: "logodesign",
     label: "Logo Design",
   },
 ];
